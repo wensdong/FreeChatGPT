cb2b1b8c2318664b49e73e42e525c438a9a937ce
wensdong <18463724+wensdong@users.noreply.github.com>
2023-03-20 07:09:44 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+base: '/REPO/',
+  /* Rest of the configuration here */
+});
   plugins: [react()],
   resolve: {
     alias: {
